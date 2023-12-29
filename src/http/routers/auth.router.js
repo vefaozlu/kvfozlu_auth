09981334942431d35aa443b893b3ad6d0794e519
Vefa Ozlu <vefaozlu01@gmail.com>
2023-12-29 20:30:22 +0300

    dada

--- src/http/routers/auth.router.js
+++ src/http/routers/auth.router.js
@@ -7,9 +7,6 @@ router
   .get(isLoggedIn, AuthController.loginView)
   .post(AuthController.login);
 
-<<<<<<< HEAD
 router.route("/login").get(AuthController.loginView);
 
-=======
->>>>>>> a8b49edcd1930297f63faf499bc747f0bc7e9545
 module.exports = router;
